b699470d8fcb7dc405217dbccba8b53f5ecc496d
Wong Zheng Quan <wong.zhengquan@gmail.com>
2019-11-09 01:41:43 +0800

    Removed gif file

--- Slap/Properties/Resources.Designer.cs
+++ Slap/Properties/Resources.Designer.cs
@@ -173,16 +173,6 @@ namespace Slap.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap giphy {
-            get {
-                object obj = ResourceManager.GetObject("giphy", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap search {
             get {
                 object obj = ResourceManager.GetObject("search", resourceCulture);
